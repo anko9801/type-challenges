91d6a11ea699acb99ca16d80380bb01dac0e3496
anko9801 <daiusa3@icloud.com>
2023-02-17 20:26:36 +0900

    solve 612

--- questions/00612-medium-kebabcase/template.ts
+++ questions/00612-medium-kebabcase/template.ts
@@ -1,1 +1,18 @@
-type KebabCase<S> = any
+type IsLetter<S extends string> = Uppercase<S> extends Lowercase<S>
+  ? false
+  : true
+type IsCapital<S extends string> = IsLetter<S> extends true
+  ? S extends Uppercase<S>
+    ? true
+    : false
+  : false
+type KebabCase<
+  S extends string,
+  IsFirst extends boolean = true,
+> = S extends `${infer L}${infer R}`
+  ? IsFirst extends true
+    ? `${Lowercase<L>}${KebabCase<R, false>}`
+    : IsCapital<L> extends true
+    ? `-${Lowercase<L>}${KebabCase<R, false>}`
+    : `${L}${KebabCase<R, false>}`
+  : ''
